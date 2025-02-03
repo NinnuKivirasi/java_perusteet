19c753a3f3cf0e61194c724e54d099059b819b8f
Nina Kivirasi <159410927+NinnuKivirasi@users.noreply.github.com>
2025-02-03 15:00:05 +0200

    Add files via upload

---
+++ osa-4/Kertoja/Kertoja.java
@@ -0,0 +1,11 @@
+public class Kertoja {
+    private int kerro;
+
+    public Kertoja(int luku) {
+        this.kerro = luku;
+    }
+
+    public int kerro(int luku) {
+        return this.kerro = luku * this.kerro;
+    }
+}
