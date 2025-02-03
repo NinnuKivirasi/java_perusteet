2ff25b47f62445a99ba7679f3210851d0a8e836e
Nina Kivirasi <159410927+NinnuKivirasi@users.noreply.github.com>
2025-02-03 14:55:54 +0200

    Add files via upload

---
+++ osa-4/ensimmainenTili/EnsimmainenTilisi.java
@@ -0,0 +1,16 @@
+
+public class EnsimmainenTilisi {
+
+    public static void main(String[] args) {
+        // Koodirivi tulee tähän
+        // Tili.Java:ssa olevaan koodiin ei tule koskea
+        // tee ohjelmasi tänne
+        Tili ensimmainenTili = new Tili("", 100.00);
+
+        System.out.println(ensimmainenTili);
+
+        ensimmainenTili.pano(20.0);
+
+        System.out.println(ensimmainenTili);
+    }
+}
